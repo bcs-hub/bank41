ff495bb6295616b810160d270f98f94c1735a39a
Rain Tüür <rain.tuur@gmail.com>
2026-08-27 10:25:56 +0300

    FEB-5 WIP 2

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from "@/views/HomeView.vue";
+import HomeView from '@/views/HomeView.vue'
 import ErrorView from '@/views/ErrorView.vue'
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
   routes: [
     {
       path: '/',
