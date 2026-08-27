52444a2c396dd5c7b434e3fbeae39969aa3e14d9
Anngrett <anngrett.sahtel@gmail.com>
2026-08-27 11:40:56 +0300

    FEB-6 Complete

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from "@/views/HomeView.vue";
 import ErrorView from "@/views/ErrorView.vue";
+import AtmsView from '@/views/AtmsView.vue'
 
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
       path: '/error',
       name: 'errorRoute',
       component: ErrorView,
+    },
+    {
+      path: '/atms',
+      name: 'atmsRoute',
+      component: AtmsView,
     },
   ],
 })
