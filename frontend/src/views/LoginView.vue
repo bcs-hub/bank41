8c502e891b464b6c65376394d16d9a61f168ada0
Gerli <gerlir002@gmail.com>
2026-08-27 16:14:12 +0300

    FEB-7-4 comlete

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,19 +1,44 @@
 <script>
 export default {
   name: 'LoginView',
+  methods: {
+    login() {
+      if (this.loginRequest.username === '' || this.loginRequest.password === '') {
+        alert('Täida kõik väljad')
+      }
+    },
+  },
+  data() {
+    return {
+      message: 'Täida kõik väljad',
+      loginRequest: {
+        username: '',
+        password: '',
+      },
+    }
+  },
 }
 </script>
 
 <template>
   <div class="container text-center">
     <div class="row justify-content-center">
-      <div class="col col-3">
+      <div class="col col-6">
+        <h4>Sisselogimine</h4>
+        ,
+        <div v-if="true" class="alert-danger" role="alert"> {{messege}}</div>
+
         <div class="form-floating mb-3">
-          <input type="text" class="form-control" placeholder="" />
+          <input v-model="loginRequest.username" type="text" class="form-control" placeholder="" />
           <label>Kasutajanimi</label>
         </div>
         <div class="form-floating">
-          <input type="password" class="form-control" placeholder="" />
+          <input
+            v-model="loginRequest.password"
+            type="password"
+            class="form-control"
+            placeholder=""
+          />
           <label>Parool</label>
         </div>
         <button type="submit" class="btn btn-primary">Logi sisse</button>
