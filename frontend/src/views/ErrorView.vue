dca0bdcfb2b77f2fcf047952685456196d7a68e4
Fred <lehingfred@gmail.com>
2026-08-27 10:30:31 +0300

    FEB-5 Complete

--- frontend/src/views/ErrorView.vue
+++ frontend/src/views/ErrorView.vue
@@ -1,9 +1,19 @@
 <script>
 export default {
-name: "ErrorView"
+  name: 'ErrorView',
 }
 </script>
 
 <template>
-  <div>TERE</div>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Oops, midagi läks valesti</h1>
+        <img
+          src="https://www.enworld.org/media/blown-mind-explosion-gif-gif.75242/full"
+          alt="pilt midagi on katki"
+        />
+      </div>
+    </div>
+  </div>
 </template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script>
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
 }
 </script>
 
@@ -10,9 +10,9 @@ export default {
       <div class="col">See on meie äge bank! :)</div>
       <div class="col">
         <img
-          src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjBhbmRkbnhkeXMyeXYzb3g4MGZraG1ydGFoNmYyaXVoYzNvdmdwYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l0DAIjfnbjj0p2F2w/giphy.gif"
-          class="img-thumbnail"
-          alt="Lahe, hoiupõrsas, lööme ta katki! "
+            src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjBhbmRkbnhkeXMyeXYzb3g4MGZraG1ydGFoNmYyaXVoYzNvdmdwYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l0DAIjfnbjj0p2F2w/giphy.gif"
+            class="img-thumbnail"
+            alt="Lahe, hoiupõrsas, lööme ta katki! "
         />
       </div>
     </div>
