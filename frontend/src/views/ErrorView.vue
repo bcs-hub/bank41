0d70436825572ed63d97336d405efdd103e28a30
Jaanus R <jaanus.riiner@gmail.com>
2026-08-27 10:37:23 +0300

    FEB-5 complete

---
+++ frontend/src/views/ErrorView.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'ErrorView',
+}
+</script>
+
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h1>Midagi läks valesti</h1>
+        <img
+          src="https://media.tenor.com/lmBb2gQ-YwgAAAAM/a-lot-of-things-went-wrong-tom-hanks.gif"
+          alt="Valesti läks lõbus GIF"
+        />
+      </div>
+    </div>
+  </div>
+</template>
