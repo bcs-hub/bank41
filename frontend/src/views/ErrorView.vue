b6b83cf5ced3cf7a4087b915fa0cf00a1879fe2b
Anastasija Selevjorstova <selevjorstovaa@gmail.com>
2026-08-27 11:59:58 +0300

    Add error view

---
+++ frontend/src/views/ErrorView.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'ErrorView',
+}
+</script>
+
+<template>
+  <div class="container text-center mt-5">
+    <h1>Midagi läks valesti 🙂</h1>
+
+    <img src="/error.gif" alt="Error" class="img-fluid mt-4" />
+  </div>
+</template>
