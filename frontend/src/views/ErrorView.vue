839fafa4794248f860324c3bbd22bfa84aed7b7c
Andrus-Lahe <andrus.lahe@gmail.com>
2026-08-27 10:50:19 +0300

    Andrus feb 5 (#3)
    * FEB-5 WIP 1
    
    * otsi gif juurde
    
    ---------
    
    Co-authored-by: Rain Tüür <rain.tuur@gmail.com>

---
+++ frontend/src/views/ErrorView.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'ErrorView',
+}
+</script>
+
+<template>
+  <div class=""container test">
+  </div> class="row"
+      <div class="col"
+        <h1 Uups midagi läks valesti</h1>
+        <img
+        src="https://media.tenor.com/nANqORN7qhQAAAAM/explosion-explode.gif"
+        alt="pilt"
+      </div>
+    </div>
+
+</template>
+
+
+
+
