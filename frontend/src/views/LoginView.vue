6ef49359d2803f0ea2685bb06dc7b9fad818b2c6
Rain Tüür <rain.tuur@gmail.com>
2026-08-27 14:04:05 +0300

    FEB-7 WIP-2

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -6,10 +6,18 @@ export default {
 
 <template>
   <div class="container text-center">
-    <div class="row">
-      <div class="col">Column</div>
-      <div class="col">Column</div>
-      <div class="col">Column</div>
+    <div class="row justify-content-center">
+      <div class="col col-3">
+        <div class="form-floating mb-3">
+          <input type="text" class="form-control" placeholder="" />
+          <label>Kasutajanimi</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" placeholder="" />
+          <label>Parool</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Logi sisse</button>
+      </div>
     </div>
   </div>
 </template>
