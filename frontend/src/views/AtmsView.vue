a34e7effea980b110c7ee87a8443a015ec7d49f6
Rain Tüür <rain.tuur@gmail.com>
2026-08-31 16:10:01 +0300

    FEB-9 WIP 1

--- frontend/src/views/AtmsView.vue
+++ frontend/src/views/AtmsView.vue
@@ -1,14 +1,19 @@
 <script>
 export default {
   name: 'AtmsView',
-  methods: {
-
-  },
   data() {
     return {
       userId: sessionStorage.getItem('userId'),
       roleName: sessionStorage.getItem('roleName'),
     }
+  },
+  methods: {
+    getCities() {
+      alert("Toon andmeid")
+    },
+  },
+  beforeMount() {
+    this.getCities()
   },
 }
 </script>
@@ -18,9 +23,20 @@ export default {
     <div class="row">
       <div class="col">
         <h1>Pangaautomaadid</h1>
-        <div ref="useridShowBox">userId: x</div>
-        <div>roleName: y</div>
       </div>
+    </div>
+
+    <div class="row">
+      <div class="col col-2">
+        <select class="form-select" aria-label="Default select example">
+          <option value="0">Kõik linnad</option>
+          <option value="2">Tallinn</option>
+          <option value="3">Tartu</option>
+          <option value="1">Pärnu</option>
+        </select>
+      </div>
+
+      <div class="col">asukohtade tabel (placeholder)</div>
     </div>
   </div>
 </template>
