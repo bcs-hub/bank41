0356ea0d5e469e8293bcd80bff3400b40fdce39b
Jaanus R <jaanus.riiner@gmail.com>
2026-09-01 15:39:25 +0300

    FEB-10 WIP 4

--- frontend/src/views/AtmsView.vue
+++ frontend/src/views/AtmsView.vue
@@ -3,15 +3,17 @@ import CityService from '@/services/CityService.js'
 import CitiesDropdown from '@/components/CitiesDropdown.vue'
 import LocationService from '@/services/LocationService.js'
 import NavigationService from '@/services/NavigationService.js'
+import AlertDanger from '@/components/AlertDanger.vue'
 
 export default {
   name: 'AtmsView',
-  components: { CitiesDropdown },
+  components: { AlertDanger, CitiesDropdown },
   data() {
     return {
       userId: sessionStorage.getItem('userId'),
       roleName: sessionStorage.getItem('roleName'),
       cityId: 0,
+      errorMessage: '',
 
       cities: [
         {
@@ -36,6 +38,10 @@ export default {
           ],
         },
       ],
+      errorResponse: {
+        message: '',
+        errorCode: '',
+      },
     }
   },
   methods: {
@@ -47,9 +53,10 @@ export default {
     },
 
     getLocations() {
+      this.errorMessage = ''
       LocationService.getLocationsRequest(this.cityId)
         .then((response) => this.handleGetLocationsResponse(response))
-        .catch(() => NavigationService.navigateToErrorView())
+        .catch((error) => this.handleGetLocationsErrorResponse(error))
         .finally()
     },
 
@@ -64,6 +71,13 @@ export default {
     handleGetLocationsResponse(response) {
       this.locations = response.data
     },
+    handleGetLocationsErrorResponse(error) {
+      this.errorResponse = error.response.data
+      if (error.response.status === 404 && this.errorResponse.errorCode === 'NO_LOCATION_FOUND') {
+        this.errorMessage = this.errorResponse.message
+        this.locations = []
+      }
+    }
   },
   beforeMount() {
     this.getCities()
@@ -74,9 +88,10 @@ export default {
 
 <template>
   <div class="container text-center">
-    <div class="row mb-4">
-      <div class="col">
+    <div class="row justify-content-center mb-4">
+      <div class="col col-5">
         <h1>Pangaautomaadid</h1>
+        <AlertDanger :error-message="errorMessage"/>
       </div>
     </div>
 
@@ -99,11 +114,14 @@ export default {
               <td>{{ location.cityName }}</td>
               <td>{{ location.locationName }}</td>
               <td>
-                  <div v-for="transactionType in location.transactionTypes" :key="transactionType.transactionTypeId">
-                    <div v-if="transactionType.isAvailable">
-                      {{transactionType.transactionTypeName}}
-                    </div>
+                <div
+                  v-for="transactionType in location.transactionTypes"
+                  :key="transactionType.transactionTypeId"
+                >
+                  <div v-if="transactionType.isAvailable">
+                    {{ transactionType.transactionTypeName }}
                   </div>
+                </div>
               </td>
             </tr>
           </tbody>
