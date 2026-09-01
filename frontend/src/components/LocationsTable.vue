6dbbe026ab43b100501bd21d51353eef6f6c0611
triin <triinlemba@gmail.com>
2026-09-01 15:47:24 +0300

    Extract LocationsTable to separate component

---
+++ frontend/src/components/LocationsTable.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: 'LocationsTable',
+  props: {
+    locations: {}
+  }
+}
+</script>
+
+<template>
+  <table class="table table-hover table-bordered table-light">
+    <thead>
+    <tr>
+      <th scope="col">Linn</th>
+      <th scope="col">Asukoht</th>
+      <th scope="col">Teenused</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="location in locations" :key="location.locationId">
+      <th scope="row">{{ location.cityName }}</th>
+      <td>{{ location.locationName }}</td>
+      <td>
+        <div
+            v-for="transactionType in location.transactionTypes"
+            :key="transactionType.transactionTypeId"
+            class="text-left"
+        >
+          <div v-if="transactionType.isAvailable">
+            {{ transactionType.transactionTypeName }}
+          </div>
+        </div>
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
--- frontend/src/views/AtmsView.vue
+++ frontend/src/views/AtmsView.vue
@@ -4,10 +4,11 @@ import CitiesDropdown from '@/components/CitiesDropdown.vue'
 import LocationService from '@/services/LocationService.js'
 import NavigationService from '@/services/NavigationService.js'
 import AlertDanger from '@/components/AlertDanger.vue'
+import LocationsTable from '@/components/LocationsTable.vue'
 
 export default {
   name: 'AtmsView',
-  components: { AlertDanger, CitiesDropdown },
+  components: { LocationsTable, AlertDanger, CitiesDropdown },
   data() {
     return {
       errorMessage: '',
@@ -94,32 +95,7 @@ export default {
         <CitiesDropdown :cities="cities" @event-new-city-selected="reloadLocationsTable" />
       </div>
       <div class="col col-5">
-        <table class="table table-hover table-bordered table-light">
-          <thead>
-            <tr>
-              <th scope="col">Linn</th>
-              <th scope="col">Asukoht</th>
-              <th scope="col">Teenused</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="location in locations" :key="location.locationId">
-              <th scope="row">{{ location.cityName }}</th>
-              <td>{{ location.locationName }}</td>
-              <td>
-                <div
-                  v-for="transactionType in location.transactionTypes"
-                  :key="transactionType.transactionTypeId"
-                  class="text-left"
-                >
-                  <div v-if="transactionType.isAvailable">
-                    {{ transactionType.transactionTypeName }}
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <LocationsTable :locations="locations" />
       </div>
     </div>
   </div>
