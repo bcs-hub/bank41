41a48576319ce770fad9811fa72311b9763a14ac
Rain Tüür <rain.tuur@gmail.com>
2026-09-01 10:18:57 +0300

    FEB-9 WIP-5

--- frontend/src/views/AtmsView.vue
+++ frontend/src/views/AtmsView.vue
@@ -1,8 +1,10 @@
 <script>
-import CityService from "@/services/CityService.js";
+import CityService from '@/services/CityService.js'
+import CitiesDropdown from '@/components/CitiesDropdown.vue'
 
 export default {
   name: 'AtmsView',
+  components: { CitiesDropdown },
   data() {
     return {
       userId: sessionStorage.getItem('userId'),
@@ -11,25 +13,22 @@ export default {
       cities: [
         {
           cityId: 0,
-          cityName: ''
-        }
-      ]
-
-
+          cityName: '',
+        },
+      ],
     }
   },
   methods: {
     getCities() {
-
       CityService.getCitiesRequest()
-          .then(response => this.handleGetCitiesResponse(response))
-          .catch()
-          .finally()
+        .then((response) => this.handleGetCitiesResponse(response))
+        .catch()
+        .finally()
     },
 
     handleGetCitiesResponse(response) {
       this.cities = response.data
-    }
+    },
   },
   beforeMount() {
     this.getCities()
@@ -47,10 +46,7 @@ export default {
 
     <div class="row">
       <div class="col col-2">
-        <select class="form-select" aria-label="Default select example">
-          <option value="0">Kõik linnad</option>
-          <option value="2">X</option>
-        </select>
+        <CitiesDropdown :cities="cities" />
       </div>
 
       <div class="col">asukohtade tabel (placeholder)</div>
