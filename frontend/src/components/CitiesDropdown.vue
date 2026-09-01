f3a775c66f383672b4fb07aac21aa5566c52e711
Andrus-Lahe <andrus.lahe@gmail.com>
2026-09-01 10:19:18 +0300

    asi2

---
+++ frontend/src/components/CitiesDropdown.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'CitiesDropdown',
+  props: {
+
+    cities: Array
+  }
+}
+</script>
+
+<template>
+  <select class="form-select" aria-label="Default select example">
+    <option :value="0">Kõik linnad</option>
+    <option v-for="city in cities" :key="city.cityId" :value="city.cityId">{{city.cityName}}</option>
+  </select>
+</template>
