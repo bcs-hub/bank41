b1a861f7da2317c2238a8ea0b6e75e694b24d597
Anngrett <anngrett.sahtel@gmail.com>
2026-09-01 15:47:09 +0300

    FEB-10 COMPLETE

--- frontend/src/services/LocationService.js
+++ frontend/src/services/LocationService.js
@@ -5,23 +5,24 @@ export default {
   getAtmLocations(cityId) {
     // todo: Kustuta prefer osa ära, kui enam Stoplighti mock backi ei kasuta
     let preferValue
+
     if (cityId === 0) {
-      preferValue = 'code=200, example=1'
+      preferValue = 'code=200, example=0'
     } else if (cityId === 2){
       preferValue = 'code=200, example=2'
     } else if (cityId === 3){
-      preferValue = 'code=403, example=3'
+      preferValue = 'code=200, example=3'
     } else {
-      preferValue = 'code=200, example=1'
+      preferValue = 'code=404, example=1'
     }
     return axios.get('/api/atm/locations', {
       headers: {
-        Prefer: preferValue
+        Prefer: preferValue,
     },
       params: {
-        cityId: cityId
+        cityId: cityId,
 
-      }
+      },
     })
 
 
--- frontend/src/views/AtmsView.vue
+++ frontend/src/views/AtmsView.vue
@@ -3,15 +3,18 @@ import CityService from '@/services/CityService.js'
 import CitiesDropdown from '@/components/CitiesDropdown.vue'
 import LocationService from '@/services/LocationService.js'
 import NavigationService from '@/services/NavigationService.js'
+import AlertDanger from '@/components/AlertDanger.vue'
+import LocationsTable from '@/components/LocationsTable.vue'
 
 export default {
   name: 'AtmsView',
-  components: { CitiesDropdown },
+  components: { LocationsTable, AlertDanger, CitiesDropdown },
   data() {
     return {
       userId: sessionStorage.getItem('userId'),
       roleName: sessionStorage.getItem('roleName'),
       cityId: 0,
+      errorMessage: '',
 
       cities: [
         {
@@ -36,6 +39,10 @@ export default {
           ],
         },
       ],
+      errorResponse: {
+        message: '',
+        errorCode: '',
+      },
     }
   },
   methods: {
@@ -51,9 +58,10 @@ export default {
     },
 
     getLocations() {
+      this.errorMessage = ''
       LocationService.getAtmLocations(this.cityId)
         .then((response) => this.handleGetLocationsResponse(response))
-        .catch()
+        .catch((error) => this.handleGetLocationsErrorResponse(error))
         .finally()
     },
 
@@ -63,6 +71,13 @@ export default {
     },
     handleGetLocationsResponse(response) {
       this.locations = response.data
+    },
+    handleGetLocationsErrorResponse(error) {
+      this.errorResponse = error.response.data
+      if (error.response.status === 404 && this.errorResponse.errorCode === 'NO_LOCATION_FOUND') {
+        this.errorMessage = this.errorResponse.message
+        this.locations = []
+      }
     },
   },
   beforeMount() {
@@ -74,41 +89,21 @@ export default {
 
 <template>
   <div class="container text-center">
-    <div class="row mb-4">
-      <div class="col">
+    <div class="row justify-content-center mb-4">
+      <div class="col col-5">
         <h1>Pangaautomaadid</h1>
+        <AlertDanger :error-message="errorMessage" />
       </div>
     </div>
 
-    <div class="row">
+    <div class="row justify-content-center">
       <div class="col col-2">
-        <CitiesDropdown :cities="cities" @event-new-city-selected="reloadLocationsTable"/>
+        <CitiesDropdown :cities="cities" @event-new-city-selected="reloadLocationsTable" />
       </div>
 
-      <div class="col">
+      <div class="col col-4">
         <!-- todo  SIIN ON ASUKOHTADE TABEL     -->
-        <table class="table table-dark table-hover">
-          <thead>
-            <tr>
-              <th scope="col">Linn</th>
-              <th scope="col">Asukoht</th>
-              <th scope="col">Teenused</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="location in locations" :key="location.locationId">
-              <td>{{ location.cityName }}</td>
-              <td>{{ location.locationName }}</td>
-              <td>
-                <div v-for="transactionType in location.transactionTypes" :key="transactionType.transactionTypeId">
-                  <div v-if="transactionType.isAvailable">
-                    {{ transactionType.transactionTypeName }}
-                  </div>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <LocationsTable :locations="locations" />
       </div>
     </div>
   </div>
