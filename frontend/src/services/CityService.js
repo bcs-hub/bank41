e8a07280b7654e5f11ab4e316f0bfa8779a8734d
triin <triinlemba@gmail.com>
2026-09-01 11:13:54 +0300

    FEB-9 completed.

---
+++ frontend/src/services/CityService.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+
+export default {
+  getCitiesRequest() {
+    return axios.get('/api/cities');
+  },
+}
--- frontend/src/views/AtmsView.vue
+++ frontend/src/views/AtmsView.vue
@@ -1,11 +1,38 @@
 <script>
+import CityService from '@/services/CityService.js'
+import CitiesDropdown from '@/components/CitiesDropdown.vue'
+
 export default {
   name: 'AtmsView',
-  methods: {},
+  components: { CitiesDropdown },
   data() {
     return {
       userId: sessionStorage.getItem('userId'),
+      cities: [
+        {
+          cityId: 0,
+          cityName: '',
+        },
+      ],
     }
+  },
+  methods: {
+    getCitites() {
+      CityService.getCitiesRequest()
+        .then((response) => this.handleGetCitiesResponse(response))
+        .catch()
+        .finally()
+    },
+    handleGetCitiesResponse(response) {
+      this.cities = response.data
+    },
+    alertWithCityId(cityId) {
+      console.log(cityId);
+      alert(cityId)
+    },
+  },
+  beforeMount() {
+    this.getCitites()
   },
 }
 </script>
@@ -15,9 +42,13 @@ export default {
     <div class="row">
       <div class="col">
         <h1>Pangaautomaadid</h1>
-        <div>userId: {{ userId }}</div>
-        <div>roleName: y</div>
       </div>
+    </div>
+    <div class="row">
+      <div class="col col-4">
+        <CitiesDropdown :cities="cities" @event-new-city-selected="alertWithCityId" />
+      </div>
+      <div class="col">placeholder tabel</div>
     </div>
   </div>
 </template>
