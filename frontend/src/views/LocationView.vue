637945a884e272c46c01e0924f742d553d5d86cd
Rain Tüür <rain.tuur@gmail.com>
2026-09-02 09:32:28 +0300

    FEB-11 WIP-1

---
+++ frontend/src/views/LocationView.vue
@@ -0,0 +1,37 @@
+<script>
+import CitiesDropdown from "@/components/CitiesDropdown.vue";
+
+export default {
+  name: 'LocationView',
+  components: {CitiesDropdown},
+}
+</script>
+
+<template>
+  <div class="container text-center">
+    <div class="row justify-content-center">
+      <div class="col">
+        <h1>Lisa asukoht</h1>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col col-2">
+        <CitiesDropdown/>
+      </div>
+      <div class="col">
+        väljad placeholder
+      </div>
+      <div class="col">
+        pilt
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col">
+        <button class="btn btn-secondary" type="submit">Tagasi</button>
+        <button class="btn btn-success" type="submit">Lisa</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
