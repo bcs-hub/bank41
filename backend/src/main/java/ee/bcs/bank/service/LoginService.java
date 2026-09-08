88e8ca10cae6f836eae42b7909f32770df5aeecc
Anngrett <anngrett.sahtel@gmail.com>
2026-09-08 21:57:57 +0300

    BEB-5 WIP 1

---
+++ backend/src/main/java/ee/bcs/bank/service/LoginService.java
@@ -0,0 +1,22 @@
+package ee.bcs.bank.service;
+
+import ee.bcs.bank.controller.LoginRequest;
+import ee.bcs.bank.persistence.user.User;
+import ee.bcs.bank.persistence.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final UserRepository userRepository;
+
+    public void loginUser(LoginRequest loginRequest) {
+        User user = userRepository.findUserByUsernameAndPasswordAndStatus(loginRequest.getUsername(), loginRequest.getPassword(), "A");
+        System.out.println("userId: " + user.getId() + " roleName" + user.getRole().getName());
+
+
+    }
+}
+
