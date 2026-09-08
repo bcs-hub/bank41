e9633bdafd9d409fce425c57741891c345ccf2d8
Rain Tüür <rain.tuur@gmail.com>
2026-09-08 14:05:18 +0300

    BEB-5 WIP-2

---
+++ backend/src/main/java/ee/bcs/bank/service/LoginService.java
@@ -0,0 +1,13 @@
+package ee.bcs.bank.service;
+
+import ee.bcs.bank.controller.LoginRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    public void loginUser(LoginRequest loginRequest) {
+
+    }
+}
