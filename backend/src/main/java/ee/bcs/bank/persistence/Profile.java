c6ffe2eaf5283b2e94e084864241dca4f9464ebc
Rain Tüür <rain.tuur@gmail.com>
2026-09-08 15:09:43 +0300

    BEB-5 WIP-3

---
+++ backend/src/main/java/ee/bcs/bank/persistence/Profile.java
@@ -0,0 +1,36 @@
+package ee.bcs.bank.persistence;
+
+import ee.bcs.bank.persistence.user.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "profile", schema = "bank")
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+
+}
--- backend/src/main/java/ee/bcs/bank/service/LoginService.java
+++ backend/src/main/java/ee/bcs/bank/service/LoginService.java
@@ -1,13 +1,18 @@
 package ee.bcs.bank.service;
 
 import ee.bcs.bank.controller.LoginRequest;
+import ee.bcs.bank.persistence.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
+
+    private final UserRepository userRepository;
+
     public void loginUser(LoginRequest loginRequest) {
 
     }
+
 }
