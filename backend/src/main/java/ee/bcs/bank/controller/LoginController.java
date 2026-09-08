992499fa61ad765732147488a1a81ac8e4273f02
Rain Tüür <rain.tuur@gmail.com>
2026-09-08 12:10:27 +0300

    BEB-5 WIP-1

---
+++ backend/src/main/java/ee/bcs/bank/controller/LoginController.java
@@ -0,0 +1,18 @@
+package ee.bcs.bank.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+
+    @PostMapping("/api/login")
+    public void loginUser(@RequestBody LoginRequest loginRequest) {
+        System.out.println("kasutajanimi: " + loginRequest.getUsername());
+        System.out.println("parool: " + loginRequest.getPassword());
+    }
+
+
+}
