4fcddc542f37e87ce5b507760bc9a982410e59ec
Rain Tüür <rain.tuur@gmail.com>
2026-09-10 15:59:51 +0300

    BEB-6 COMPLETE

---
+++ backend/src/main/java/ee/bcs/bank/controller/city/dto/CityDto.java
@@ -0,0 +1,16 @@
+package ee.bcs.bank.controller.city.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CityDto implements Serializable {
+    private Integer cityId;
+    private String cityName;
+}
