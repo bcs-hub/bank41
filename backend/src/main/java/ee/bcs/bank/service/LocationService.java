662aa37b14b51ba7973126bd941c746d95f8f79d
tiina-re <tiina.reintop@gmail.com>
2026-09-11 13:37:40 +0300

    TIINA-BEB-7 DONE

--- backend/src/main/java/ee/bcs/bank/service/LocationService.java
+++ backend/src/main/java/ee/bcs/bank/service/LocationService.java
@@ -1,8 +1,17 @@
 package ee.bcs.bank.service;
 
+import ee.bcs.bank.controller.location.dto.LocationInfo;
+import ee.bcs.bank.infrastructure.exception.DataNotFoundException;
+import ee.bcs.bank.persistence.location.Location;
+import ee.bcs.bank.persistence.location.LocationMapper;
 import ee.bcs.bank.persistence.location.LocationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static ee.bcs.bank.Error.NO_LOCATION_FOUND;
+import static ee.bcs.bank.Status.STATUS_ACTIVE;
 
 @Service
 @RequiredArgsConstructor
@@ -10,8 +19,17 @@ import org.springframework.stereotype.Service;
 public class LocationService {
 
     private final LocationRepository locationRepository;
+    private final LocationMapper locationMapper;
 
-    public void findAtmLocations(Integer cityId) {
+    public List<LocationInfo> findAtmLocations(Integer cityId) {
+
+        List<Location> locations = locationRepository.findFilteredLocationsBy(cityId, STATUS_ACTIVE.getCode());
+        if (locations.isEmpty()) {
+            throw new DataNotFoundException(NO_LOCATION_FOUND.getMessage(), NO_LOCATION_FOUND.name());
+        }
+
+        List<LocationInfo> locationInfos = locationMapper.toLocationInfos(locations);
+        return locationInfos;
 
     }
 }
