9993b72bb472ed9e957d0fb0563326500e2bca49
tiina-re <tiina.reintop@gmail.com>
2026-09-11 10:49:19 +0300

    TIINA-BEB-7 + LocationController + LocationService + LocationRepository + packages sorteerimine

---
+++ backend/src/main/java/ee/bcs/bank/persistence/location/LocationRepository.java
@@ -0,0 +1,13 @@
+package ee.bcs.bank.persistence.location;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+
+    @Query("select l from Location l where l.city.id = :id and l.status = :status order by l.city.name, l.name")
+    List<Location> findByCity_IdAndStatusOrderByCity_NameAscNameAsc(@Param("id") Integer id, @Param("status") String status);
+}
---
+++ backend/src/main/java/ee/bcs/bank/service/LocationService.java
@@ -0,0 +1,17 @@
+package ee.bcs.bank.service;
+
+import ee.bcs.bank.persistence.location.LocationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+
+    public void findAtmLocations(Integer cityId) {
+
+    }
+}
