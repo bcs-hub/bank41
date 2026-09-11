3746ce44ec4c20387b31a0a4819c63d5192513b7
triin <triinlemba@gmail.com>
2026-09-11 13:38:52 +0300

    BEB-7 completed.

--- backend/src/main/java/ee/bcs/bank/Error.java
+++ backend/src/main/java/ee/bcs/bank/Error.java
@@ -4,13 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum Error {
-    INCORRECT_CREDENTIALS("Vale kasutajanimi või parool");
+    INCORRECT_CREDENTIALS("Vale kasutajanimi või parool"),
+    NO_LOCATION_FOUND("Ei leitud ühtegi pangaautomaati");
 
     private final String message;
 
     Error(String message) {
         this.message = message;
     }
-
-
 }
