ac2e6273f2cf940b133446e0a54d9f413c138698
aivartnsm <aivartnsm@gmail.com>
2026-09-11 15:33:50 +0300

    BEB-8 WIP 2

--- backend/src/main/java/ee/bcs/bank/persistence/locationtransactiontype/LocationTransactionTypeRepository.java
+++ backend/src/main/java/ee/bcs/bank/persistence/locationtransactiontype/LocationTransactionTypeRepository.java
@@ -2,7 +2,6 @@ package ee.bcs.bank.persistence.locationtransactiontype;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface LocationTransactionTypeRepository extends JpaRepository<LocationTransactionType, Integer> {
     @Query("""
