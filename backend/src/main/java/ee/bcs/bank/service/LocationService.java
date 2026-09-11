3ace1ecb192be0f10e3e758ebd8cdf1634690e08
aivartnsm <aivartnsm@gmail.com>
2026-09-11 11:47:27 +0300

    BEB-7 WIP

--- backend/src/main/java/ee/bcs/bank/service/LocationService.java
+++ backend/src/main/java/ee/bcs/bank/service/LocationService.java
@@ -1,8 +1,13 @@
 package ee.bcs.bank.service;
 
+import ee.bcs.bank.persistence.location.Location;
 import ee.bcs.bank.persistence.location.LocationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static ee.bcs.bank.Status.STATUS_ACTIVE;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +16,6 @@ public class LocationService {
     private final LocationRepository locationRepository;
 
     public void findAtmLocations(Integer cityId) {
-        locationRepository
+        List<Location> locations = locationRepository.findFilteredLocationsBy(cityId, STATUS_ACTIVE.getCode());
     }
 }
