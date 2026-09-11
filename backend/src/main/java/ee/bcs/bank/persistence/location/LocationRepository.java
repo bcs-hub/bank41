b8abbe92802b7b3eaea798f795bd821923268070
Anngrett <anngrett.sahtel@gmail.com>
2026-09-11 13:38:38 +0300

    BEB-7 COMPLETE

---
+++ backend/src/main/java/ee/bcs/bank/persistence/location/LocationRepository.java
@@ -0,0 +1,13 @@
+package ee.bcs.bank.persistence.location;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+
+
+    @Query("select l from Location l where (:cityId = 0 or l.city.id = :cityId) and l.status = :status order by l.city.name, l.name")
+    List<Location> findFilteredLocationsBy(Integer cityId, String status);
+}
