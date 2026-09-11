645259e7abeb117f04668fe2b720e7836765466f
aivartnsm <aivartnsm@gmail.com>
2026-09-11 13:38:47 +0300

    BEB-7 Complete

--- backend/src/main/java/ee/bcs/bank/controller/location/LocationController.java
+++ backend/src/main/java/ee/bcs/bank/controller/location/LocationController.java
@@ -1,8 +1,13 @@
 package ee.bcs.bank.controller.location;
 
 import ee.bcs.bank.controller.location.dto.LocationInfo;
+import ee.bcs.bank.infrastructure.error.ApiError;
 import ee.bcs.bank.service.LocationService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +27,23 @@ public class LocationController {
             summary = "Tagastab pangaautomaatide asukohtade infot",
             description = "Kui cityId on 0, siis tagastatakse kõik asukohad"
     )
+    @ApiResponses(value = {
+
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK"
+            ),
+
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Kui ühtegi asukohta ei leita, siis 'message': Ei leitud ühtegi pangaautomaati, 'errorCode:' NO_LOCATION_FOUND",
+                    content = @Content(schema = @Schema(implementation = ApiError.class))
+
+            )
+    }
+    )
+
+
     public List<LocationInfo> findAtmLocations(@RequestParam Integer cityId) {
         List<LocationInfo> locationInfos = locationService.findAtmLocations(cityId);
 
