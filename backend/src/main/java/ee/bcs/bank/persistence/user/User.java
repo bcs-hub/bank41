bc61be4e614a28889c952b55a9ccccfec4f9ca33
aivartnsm <aivartnsm@gmail.com>
2026-09-11 11:07:00 +0300

    BEB-7 WIP

--- backend/src/main/java/ee/bcs/bank/persistence/user/User.java
+++ backend/src/main/java/ee/bcs/bank/persistence/user/User.java
@@ -1,6 +1,6 @@
 package ee.bcs.bank.persistence.user;
 
-import ee.bcs.bank.persistence.Role;
+import ee.bcs.bank.persistence.role.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
---
+++ backend/src/main/java/ee/bcs/bank/service/LocationService.java
@@ -0,0 +1,16 @@
+package ee.bcs.bank.service;
+
+import ee.bcs.bank.persistence.location.LocationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+
+    public void findAtmLocations(Integer cityId) {
+        locationRepository
+    }
+}
