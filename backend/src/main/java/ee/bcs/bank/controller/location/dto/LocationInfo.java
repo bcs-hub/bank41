ec7d520038818bb3565be8011d7ac4f14dfe945f
opilane_Karli <cours885@gmail.com>
2026-09-11 12:27:59 +0300

    BEB-7

---
+++ backend/src/main/java/ee/bcs/bank/controller/location/dto/LocationInfo.java
@@ -0,0 +1,21 @@
+package ee.bcs.bank.controller.location.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link ee.bcs.bank.persistence.location.Location}
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LocationInfo implements Serializable {
+    private Integer locationId;
+    private String cityName;
+    private String locationName;
+}
--- backend/src/main/java/ee/bcs/bank/service/LocationService.java
+++ backend/src/main/java/ee/bcs/bank/service/LocationService.java
@@ -1,16 +1,29 @@
 package ee.bcs.bank.service;
 
+import ee.bcs.bank.Status;
+import ee.bcs.bank.controller.location.dto.LocationInfo;
+import ee.bcs.bank.persistence.location.Location;
+import ee.bcs.bank.persistence.location.LocationMapper;
 import ee.bcs.bank.persistence.location.LocationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static ee.bcs.bank.Status.STATUS_ACTIVE;
 
 @Service
 @RequiredArgsConstructor
 public class LocationService {
 
     private final LocationRepository locationRepository;
+    private final LocationMapper locationMapper;
 
     public void findAtmLocations(Integer cityId) {
+
+        List<Location> locations = locationRepository.findFilteredLocationsBy(cityId, STATUS_ACTIVE.getCode());
+        List<LocationInfo> locationsInfos = locationMapper.toLocationsInfos(locations);
+        return locationsInfos;
 
     }
 }
