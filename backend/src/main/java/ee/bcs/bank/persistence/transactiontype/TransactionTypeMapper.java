598572c911c9ded7f70686e853e81b3c052b21aa
Fred <lehingfred@gmail.com>
2026-09-11 16:02:49 +0300

    BEB-8 Complete

--- backend/src/main/java/ee/bcs/bank/persistence/transactiontype/TransactionTypeMapper.java
+++ backend/src/main/java/ee/bcs/bank/persistence/transactiontype/TransactionTypeMapper.java
@@ -1,7 +1,12 @@
 package ee.bcs.bank.persistence.transactiontype;
 
 import ee.bcs.bank.controller.location.dto.TransactionTypeDto;
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface TransactionTypeMapper {
@@ -10,6 +15,9 @@ public interface TransactionTypeMapper {
     @Mapping(source = "name", target = "transactionTypeName")
     @Mapping(constant = "false", target = "isAvailable")
     TransactionTypeDto toTransactionTypeDto(TransactionType transactionType);
+
+
+    List<TransactionTypeDto> toTransactionTypeDtos(List<TransactionType> transactionTypes);
 
 
 }
--- backend/src/main/java/ee/bcs/bank/service/LocationService.java
+++ backend/src/main/java/ee/bcs/bank/service/LocationService.java
@@ -2,11 +2,14 @@ package ee.bcs.bank.service;
 
 
 import ee.bcs.bank.controller.location.dto.LocationInfo;
+import ee.bcs.bank.controller.location.dto.TransactionTypeDto;
 import ee.bcs.bank.infrastructure.exception.DataNotFoundException;
 import ee.bcs.bank.persistence.location.Location;
 import ee.bcs.bank.persistence.location.LocationMapper;
 import ee.bcs.bank.persistence.location.LocationRepository;
+import ee.bcs.bank.persistence.locationtransactiontype.LocationTransactionTypeRepository;
 import ee.bcs.bank.persistence.transactiontype.TransactionType;
+import ee.bcs.bank.persistence.transactiontype.TransactionTypeMapper;
 import ee.bcs.bank.persistence.transactiontype.TransactionTypeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
@@ -25,21 +28,39 @@ public class LocationService {
     private final LocationRepository locationRepository;
     private final LocationMapper locationMapper;
     private final TransactionTypeRepository transactionTypeRepository;
+    private final TransactionTypeMapper transactionTypeMapper;
+    private final LocationTransactionTypeRepository locationTransactionTypeRepository;
 
     public List<LocationInfo> findAtmLocations(Integer cityId) {
         List<Location> locations = locationRepository.findFilteredLocationsBy(cityId, STATUS_ACTIVE.getCode());
+        validateAtLestOneLocationExists(locations);
+        List<LocationInfo> locationInfos = locationMapper.toLocationInfos(locations);
+        addTransactionTypes(locationInfos);
+        return locationInfos;
+    }
+
+    private void addTransactionTypes(List<LocationInfo> locationInfos) {
+        for (LocationInfo locationinfo : locationInfos) {
+            List<TransactionTypeDto> transactionTypeDtos = createTransactionTypeDtos(locationinfo);
+            locationinfo.setTransactionTypes(transactionTypeDtos);
+        }
+    }
+
+    private List<TransactionTypeDto> createTransactionTypeDtos(LocationInfo locationinfo) {
+        Sort byNameDesc = Sort.by(Sort.Direction.DESC, "name");
+        List<TransactionType> transactionTypes = transactionTypeRepository.findAll(byNameDesc);
+        List<TransactionTypeDto> transactionTypeDtos = transactionTypeMapper.toTransactionTypeDtos(transactionTypes);
+
+        for (TransactionTypeDto transactionTypeDto : transactionTypeDtos) {
+            boolean locationTransactionTypeExists = locationTransactionTypeRepository.locationTransactionTypeExistsBy(locationinfo.getLocationId(), transactionTypeDto.getTransactionTypeId());
+            transactionTypeDto.setIsAvailable(locationTransactionTypeExists);
+        }
+        return transactionTypeDtos;
+    }
+
+    private static void validateAtLestOneLocationExists(List<Location> locations) {
         if (locations.isEmpty()) {
             throw new DataNotFoundException(NO_LOCATION_FOUND.getMessage(), NO_LOCATION_FOUND.name());
         }
-        List<LocationInfo> locationInfos = locationMapper.toLocationInfos(locations);
-
-        for (LocationInfo locationinfo : locationInfos) {
-            Sort byNameDesc = Sort.by(Sort.Direction.DESC, "name");
-            List<TransactionType> transactionTypes = transactionTypeRepository.findAll(byNameDesc);
-
-        }
-
-        return locationInfos;
-
     }
 }
