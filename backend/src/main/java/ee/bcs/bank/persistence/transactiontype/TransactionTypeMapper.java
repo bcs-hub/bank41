26cdb80391d74c00b07845db8212b72ec744a4b6
Rain Tüür <rain.tuur@gmail.com>
2026-09-11 14:51:17 +0300

    BEB-8 WIP-1

---
+++ backend/src/main/java/ee/bcs/bank/persistence/transactiontype/TransactionTypeMapper.java
@@ -0,0 +1,15 @@
+package ee.bcs.bank.persistence.transactiontype;
+
+import ee.bcs.bank.controller.location.dto.TransactionTypeDto;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+public interface TransactionTypeMapper {
+
+
+    @Mapping(source = "id", target = "transactionTypeId")
+    @Mapping(source = "name", target = "transactionTypeName")
+    @Mapping(constant = "false", target = "isAvailable")
+    TransactionTypeDto toTransactionTypeDto(TransactionType transactionType);
+
+}
