cc10d3dc4a88953858079322353bdc62b2122cd7
Rain Tüür <rain.tuur@gmail.com>
2026-09-11 16:03:00 +0300

    BEB-8 COMPLETE

--- backend/src/main/java/ee/bcs/bank/service/LocationService.java
+++ backend/src/main/java/ee/bcs/bank/service/LocationService.java
@@ -1,13 +1,17 @@
 package ee.bcs.bank.service;
 
 import ee.bcs.bank.controller.location.dto.LocationInfo;
+import ee.bcs.bank.controller.location.dto.TransactionTypeDto;
 import ee.bcs.bank.infrastructure.exception.DataNotFoundException;
 import ee.bcs.bank.persistence.location.Location;
 import ee.bcs.bank.persistence.location.LocationMapper;
 import ee.bcs.bank.persistence.location.LocationRepository;
+import ee.bcs.bank.persistence.locationtransasctiontype.LocationTransactionTypeRepository;
 import ee.bcs.bank.persistence.transactiontype.TransactionType;
+import ee.bcs.bank.persistence.transactiontype.TransactionTypeMapper;
 import ee.bcs.bank.persistence.transactiontype.TransactionTypeRepository;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -23,35 +27,40 @@ public class LocationService {
     private final LocationRepository locationRepository;
     private final LocationMapper locationMapper;
     private final TransactionTypeRepository transactionTypeRepository;
+    private final TransactionTypeMapper transactionTypeMapper;
+    private final LocationTransactionTypeRepository locationTransactionTypeRepository;
 
     public List<LocationInfo> findAtmLocations(Integer cityId) {
         List<Location> locations = locationRepository.findFilteredLocationsBy(cityId, STATUS_ACTIVE.getCode());
+        validateAtLeastOneLocationExists(locations);
+        List<LocationInfo> locationInfos = locationMapper.toLocationInfos(locations);
+        addTransactionTypes(locationInfos);
+        return locationInfos;
+    }
+
+    private void addTransactionTypes(List<LocationInfo> locationInfos) {
+        for (LocationInfo locationInfo : locationInfos) {
+            List<TransactionTypeDto> transactionTypeDtos = createTransactionTypeDtos(locationInfo);
+            locationInfo.setTransactionTypes(transactionTypeDtos);
+        }
+    }
+
+    private List<TransactionTypeDto> createTransactionTypeDtos(LocationInfo locationInfo) {
+        Sort byNameDesc = Sort.by(Sort.Direction.DESC, "name");
+        List<TransactionType> transactionTypes = transactionTypeRepository.findAll(byNameDesc);
+        List<TransactionTypeDto> transactionTypeDtos = transactionTypeMapper.toTransactionTypeDtos(transactionTypes);
+
+        for (TransactionTypeDto transactionTypeDto : transactionTypeDtos) {
+            boolean locationTransactionTypeExists = locationTransactionTypeRepository.locationTransactionTypeExistsBy(locationInfo.getLocationId(), transactionTypeDto.getTransactionTypeId());
+            transactionTypeDto.setIsAvailable(locationTransactionTypeExists);
+        }
+        return transactionTypeDtos;
+    }
+
+    private static void validateAtLeastOneLocationExists(List<Location> locations) {
         if (locations.isEmpty()) {
             throw new DataNotFoundException(NO_LOCATION_FOUND.getMessage(), NO_LOCATION_FOUND.name());
         }
-        List<LocationInfo> locationInfos = locationMapper.toLocationInfos(locations);
-
-
-//
-//        {
-//                "locationId": 1,
-//                "cityName": "Tallinn",
-//                "locationName": "Sikupilli Prisma",
-//                "lng": 24.7795,
-//                "lat": 59.4369,
-//                "transactionTypes": null
-//        }
-
-
-        for (LocationInfo locationInfo : locationInfos) {
-
-            Sort byNameDesc = Sort.by(Sort.Direction.DESC, "name");
-
-            List<TransactionType> transactionTypes = transactionTypeRepository.findAll(byNameDesc);
-        }
-
-
-        return locationInfos;
     }
 
 
