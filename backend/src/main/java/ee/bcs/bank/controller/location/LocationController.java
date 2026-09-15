8f82728e1d57951ce054e1bb9d878c396749d68c
Rain Tüür <rain.tuur@gmail.com>
2026-09-15 10:07:04 +0300

    BEB-9 complete

--- backend/src/main/java/ee/bcs/bank/controller/location/LocationController.java
+++ backend/src/main/java/ee/bcs/bank/controller/location/LocationController.java
@@ -28,6 +28,21 @@ public class LocationController {
                     Pildi puudumisel saadetakse imageData väärtuseks tühi string.
                     transactionTypeName infot koodis ei kasutata."""
     )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "OK"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "'message': Sellise nimega pangaautomaadi asukoht on juba süsteemis olemas, 'errorCode:' LOCATION_UNAVAILABLE",
+                    content = @Content(schema = @Schema(implementation = ApiError.class))
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "'message': Ei leidnud primary keyd 'x' väärtusega 'y', 'errorCode:' PRIMARY_KEY_NOT_FOUND",
+                    content = @Content(schema = @Schema(implementation = ApiError.class))
+            )
+    })
     public void addLocation(@RequestBody LocationDto locationDto) {
         locationService.addLocation(locationDto);
     }
